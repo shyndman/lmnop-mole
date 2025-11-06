b365d55f3d77014a393ebbb5c83481ecf24fd2ad
Scott Hyndman <scotty.hyndman@gmail.com>
2025-11-06 16:12:27 -0500

    Expand Jest configuration for DOM tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
+  }
 };
